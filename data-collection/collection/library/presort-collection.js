878525a7186c338a82733e3a22cb04668e15df6e
Nik Jeleniauskas <nikolas.jeleniauskas@gmail.com>
2025-04-07 20:00:27 -0400

    adjusted sort fn to account for non-viable values

--- data-collection/collection/library/presort-collection.js
+++ data-collection/collection/library/presort-collection.js
@@ -2,7 +2,7 @@
  * Sort an array of objects by a top-level object property.
  * @param {array} array 
  * @param {object} params 
- * @param {object} params.prop - The sorting property 
+ * @param {object} params.prop - The sorting property (string, or number)
  * @param {object} params.direction - The sort direction, "asc" or "desc".
  * @returns array
  */
@@ -11,8 +11,19 @@ function presortCollection(array, params) {
 	const prop = params.prop;
 	const direction = params.direction;
 	const result = array.sort((a, b) => {
-		if (a[prop] > b[prop]) { return 1 }
-		if (a[prop] < b[prop]) { return -1 }
+		let aType = typeof(a[prop]);
+		let bType = typeof(b[prop]);
+
+		if (aType === bType) {
+			if (a[prop] > b[prop]) { return 1 }
+			if (a[prop] < b[prop]) { return -1 }
+		} else {
+			const isEmpty = val => val === null || val === undefined || val === '';
+	
+			if (isEmpty(a[prop]) && isEmpty(b[prop])) { return 0 }
+			if (isEmpty(a[prop]) && !isEmpty(b[prop])) { return 1 }
+			if (!isEmpty(a[prop]) && isEmpty(b[prop])) { return -1 }
+		}
 	});
 
 	if (direction === 'desc') {
